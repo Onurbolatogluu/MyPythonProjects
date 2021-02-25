e0f56499981a31ec1821cc1e4ebf3a7b8212fc03
onur.bolatoglu <onur.bolatoglu@saglayici.com>
2021-02-25 23:07:35 +0300

    Factorial V:2

--- factorial.py
+++ factorial.py
@@ -4,15 +4,15 @@ Faktoriyel Bulma Programı
 Programdan çıkmak için 'q' ya basın.
 *******************""")
 while True:
-    sayi =  input("Sayı:")
+    sayi = input("Sayı:")
     if (sayi == "q"):
         print("Programdan çıkılıyor...")
         break
     sayi = int(sayi)
 
     faktoriyel = 1
-    for i in range(2,sayi+1):
+    for i in range(2, sayi+1):
+        print("Faktoriyel", faktoriyel, "i:",i)
         faktoriyel *= i
 
-    print("Faktoriyel:",faktoriyel)
-
+    print("Faktoriyel:", faktoriyel)
